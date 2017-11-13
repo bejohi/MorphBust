3f5e0381fae3d0e9aa7128c8bf6a2d78e6f6de33
Jonas Hielscher <benedikt.hielscher@st.ovgu.de>
2017-11-13 21:49:02 +0100

    ADDED: Some new functions to get the real data from the calculated results.

--- morph_bust/result_calculator.py
+++ morph_bust/result_calculator.py
@@ -15,6 +15,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+import sys
 
 
 def extract_img_info_from_result_file(file_path: str, reduced_set: bool = False):
@@ -39,8 +40,8 @@ def extract_img_info_from_result_file(file_path: str, reduced_set: bool = False)
         semicolon_split = line.split(";")
         lbp_relevant_pixel = int(semicolon_split[1])
         pixel_counter = int(semicolon_split[2])
-        average = lbp_relevant_pixel / (pixel_counter / 100)
-        information_arr.append([img_nr_1, img_nr_2, lbp_relevant_pixel, pixel_counter, average])
+        percentage = lbp_relevant_pixel / (pixel_counter / 100)
+        information_arr.append([img_nr_1, img_nr_2, lbp_relevant_pixel, pixel_counter, percentage])
     return information_arr
 
 
@@ -91,6 +92,34 @@ def calculate_differences_between_morphs_and_origin(morph_info_list: list, origi
     return difference_list
 
 
+def get_min_max_and_average_from_difference_list(difference_list: list):
+    minimum = sys.maxsize
+    maximum = 0
+    counter_1 = 0
+    counter_2 = 0
+    for diff in difference_list:
+        if diff[1] < minimum:
+            minimum = diff[1]
+        if diff[2] < minimum:
+            minimum = diff[2]
+        if diff[1] > maximum:
+            maximum = diff[1]
+        if diff[2] > maximum:
+            maximum = diff[2]
+        counter_1 += diff[1]
+        counter_2 += diff[2]
+    return [minimum, maximum, counter_1 / len(difference_list), counter_2 / len(difference_list)]
+
+
+def compare_morphs_with_golden_line(info_list: list, border_value):
+    counter = 0
+    for info in info_list:
+        if info[4] >= border_value:
+            counter += 1
+
+    return ["Higher or equal: " + str(counter), "Less: " + str(len(info_list) - counter)]
+
+
 if __name__ == "__main__":
     __complete_path = "../result_data/image_scan_result_complete_07.11.2017.csv"
     __combined_path = "../result_data/image_scan_result_combined_11.11.2017.csv"
@@ -101,7 +130,8 @@ if __name__ == "__main__":
     splicing_info = extract_img_info_from_result_file(__splicing_path)
     neutral_info = extract_img_info_from_result_file(__neutral_path, True)
 
-    print("Neutral average: " + str(get_overall_average_of_list(neutral_info)) + " with " + str(
+    neutral_average = get_overall_average_of_list(neutral_info)
+    print("Neutral average: " + str(neutral_average) + " with " + str(
         len(neutral_info)) + " images")
     print("Splicing average: " + str(get_overall_average_of_list(splicing_info)) + " with " + str(
         len(splicing_info)) + " images")
@@ -113,6 +143,19 @@ if __name__ == "__main__":
     complete_diff = calculate_differences_between_morphs_and_origin(complete_info, neutral_info)
     combined_diff = calculate_differences_between_morphs_and_origin(combined_info, neutral_info)
     splicing_diff = calculate_differences_between_morphs_and_origin(splicing_info, neutral_info)
-    print(str(len(complete_diff)))
-    print(str(len(combined_diff)))
-    print(str(len(splicing_diff)))
+
+    complete_diff_result = get_min_max_and_average_from_difference_list(complete_diff)
+    combined_diff_result = get_min_max_and_average_from_difference_list(combined_diff)
+    splicing_diff_result = get_min_max_and_average_from_difference_list(splicing_diff)
+    print("Complete diff: " + str(complete_diff_result))
+    print("Combined diff: " + str(combined_diff_result))
+    print("Splicing diff: " + str(splicing_diff_result))
+
+    complete_golden_line = neutral_average / ((complete_diff_result[2] + complete_diff_result[3]) / 2)
+    print("Complete golden line : " + str(compare_morphs_with_golden_line(complete_info, complete_golden_line)))
+
+    combined_golden_line = neutral_average / ((combined_diff_result[2] + combined_diff_result[3]) / 2)
+    print("Combined golden line: " + str(compare_morphs_with_golden_line(combined_info, combined_golden_line)))
+
+    splicing_golden_line = neutral_average / ((splicing_diff_result[2] + splicing_diff_result[3]) / 2)
+    print("Splicing golden line: " + str(compare_morphs_with_golden_line(splicing_info, splicing_golden_line)))
